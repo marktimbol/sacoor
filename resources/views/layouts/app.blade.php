0f702a68ef030bbb2c02931c8702cc7999245dc1
Mark <mark@Marks-MacBook-Pro.local>
2016-07-13 13:08:21 +0400

    Polishing homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,7 @@
                     <div class="col-md-3">
                         <figure class="Services__service">
                             <figcaption>
-                                <img src="/images/icon-coffee.png" alt="Coffee" title="Coffee" class="img-responsive" />
+                                <img src="/images/icon-instant.png" alt="Instant" title="Instant" class="img-responsive" />
                                 <h4 class="Services__service--title">Instant</h4>
                                 <p class="Services__service--description">
                                     Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.
@@ -81,7 +81,7 @@
                     <div class="col-md-3">
                         <figure class="Services__service">
                             <figcaption>
-                                <img src="/images/icon-coffee.png" alt="Coffee" title="Coffee" class="img-responsive" />
+                                <img src="/images/icon-serious.png" alt="Serious" title="Serious" class="img-responsive" />
                                 <h4 class="Services__service--title">Serious</h4>
                                 <p class="Services__service--description">
                                     Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.
@@ -93,7 +93,7 @@
                     <div class="col-md-3">
                         <figure class="Services__service">
                             <figcaption>
-                                <img src="/images/icon-coffee.png" alt="Coffee" title="Coffee" class="img-responsive" />
+                                <img src="/images/icon-frame.png" alt="Frame" title="Frame" class="img-responsive" />
                                 <h4 class="Services__service--title">Frame</h4>
                                 <p class="Services__service--description">
                                     Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.
@@ -108,52 +108,80 @@
         <div class="Portfolio">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-12">
-                        <main class="cd-main-content">
-                            <div class="cd-tab-filter-wrapper">
-                                <div class="cd-tab-filter">
-                                    <ul class="cd-filters">
-                                        <li class="placeholder"> 
-                                            <a data-type="all" href="#0">All</a> <!-- selected option on mobile -->
-                                        </li> 
-                                        <li class="filter">
-                                            <a class="selected" href="#0" data-type="all">All</a>
-                                        </li>
-                                        <li class="filter" data-filter=".color-1">
-                                            <a href="#0" data-type="color-1">Color 1</a>
-                                        </li>
-                                        <li class="filter" data-filter=".color-2">
-                                            <a href="#0" data-type="color-2">Color 2</a>
-                                        </li>
-                                    </ul> <!-- cd-filters -->
-                                </div> <!-- cd-tab-filter -->
-                            </div> <!-- cd-tab-filter-wrapper -->
-
+                    <div class="cd-main-content">
+                        <div class="Portfolio__header">
+                            <div class="col-md-6">
+                                <h3 class="Portfolio__title">Portfolio</h3>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="cd-tab-filter-wrapper">
+                                    <div class="Portfolio__filters cd-tab-filter">
+                                        <ul class="cd-filters">
+                                            <li class="placeholder"> 
+                                                <a data-type="all" href="#0">All</a> <!-- selected option on mobile -->
+                                            </li> 
+                                            <li class="filter">
+                                                <a class="selected" href="#0" data-type="all">All</a>
+                                            </li>
+                                            <li class="filter" data-filter=".color-1">
+                                                <a href="#0" data-type="color-1">Panoramas</a>
+                                            </li>
+                                            <li class="filter" data-filter=".color-2">
+                                                <a href="#0" data-type="color-2">Portraits</a>
+                                            </li>
+                                            <li class="filter" data-filter=".color-3">
+                                                <a href="#0" data-type="color-3">Macro</a>
+                                            </li>
+                                            <li class="filter" data-filter=".color-4">
+                                                <a href="#0" data-type="color-4">Journal</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
                             <section class="cd-gallery">
                                 <ul>
-                                    <li class="mix color-1 check1 radio2 option3">
-                                        <img src="http://placehold.it/270x270" alt="Image 1">
+                                    <li class="mix color-1">
+                                        <a href="#">
+                                            <img src="/images/image_01.jpg" alt="Image 1">
+                                        </a>
                                     </li>
-                                    <li class="mix color-2 check2 radio2 option2">
-                                        <img src="http://placehold.it/270x270" alt="Image 1">
+                                    <li class="mix color-2">
+                                        <a href="#">
+                                           <img src="/images/image_02.jpg" alt="Image 1">
+                                        </a>
                                     </li>
-                                    <li class="mix color-1 check3 radio3 option1">
-                                        <img src="http://placehold.it/270x270" alt="Image 1">
+                                    <li class="mix color-3">
+                                        <a href="#">
+                                            <img src="/images/image_03.jpg" alt="Image 1">
+                                        </a>
                                     </li>
-                                    <li class="mix color-1 check3 radio2 option4">
-                                        <img src="http://placehold.it/270x270" alt="Image 1">
+                                    <li class="mix color-1">
+                                        <a href="#">
+                                            <img src="/images/image_04.jpg" alt="Image 1">
+                                        </a>
                                     </li>
-                                    <li class="mix color-1 check1 radio3 option2">
-                                        <img src="http://placehold.it/270x270" alt="Image 1">
+                                    <li class="mix color-4">
+                                        <a href="#">
+                                            <img src="/images/image_05.jpg" alt="Image 1">
+                                        </a>
                                     </li>
-                                    <li class="mix color-2 check2 radio3 option3">
-                                        <img src="http://placehold.it/270x270" alt="Image 1">
+                                    <li class="mix color-2">
+                                        <a href="#">
+                                            <img src="/images/image_06.jpg" alt="Image 1">
+                                        </a>
                                     </li>
-                                    <li class="mix color-2 check2 radio2 option1">
-                                        <img src="http://placehold.it/270x270" alt="Image 1">
+                                    <li class="mix color-2">
+                                        <a href="#">
+                                            <img src="/images/image_07.jpg" alt="Image 1">
+                                        </a>
                                     </li>
-                                    <li class="mix color-1 check1 radio3 option4">
-                                        <img src="http://placehold.it/270x270" alt="Image 1">
+                                    <li class="mix color-1">
+                                        <a href="#">
+                                            <img src="/images/image_08.jpg" alt="Image 1">
+                                        </a>
                                     </li>
 
                                     <li class="gap"></li>
@@ -162,58 +190,30 @@
                                 </ul>
                                 <div class="cd-fail-message">No results found</div>
                             </section> <!-- cd-gallery -->
-                        </main>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="Portfolio">
+        <div class="DefaultSection">
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <h3 class="Portfolio__title">Portfolio</h3>
-                    </div>
-
-                    <div class="col-md-6">
-                        <div class="Portfolio__filters">
-                            <ul>
-                                <li><a href="#">All</a></li>
-                                <li><a href="#">Panoramas</a></li>
-                                <li><a href="#">Portraits</a></li>
-                                <li><a href="#">Macro</a></li>
-                                <li><a href="#">Journal</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    @foreach( range(1,8) as $index )
-                    <div class="col-md-3 Portfolio__item">
-                        <div class="Portfolio__image">
-                            <img src="http://placehold.it/270x270" alt="Portfolio title" title="Portfolio title" class="img-responsive" />
-                        </div>
-                    </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-
-        <div class="Default">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h3>Just default section</h3>
+                        <h3 class="DefaultSection__title">Just default section</h3>
                         <p>
                             Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.
                         </p>
                         <p>
                             Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.
                         </p>
+                        <p class="DefaultSection__action">
+                            <a href="#" class="button button--white">Visit me</a>
+                        </p>
                     </div>
 
                     <div class="col-md-6">
-                        <img src="http://placehold.it/563x313" alt="Title" title="Title" class="img-responsive" />
+                        <iframe src="https://player.vimeo.com/video/124208594" width="563" height="313" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
@@ -223,7 +223,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h3>Twitter</h3>
+                        <h3 class="Footer__title">Twitter</h3>
                         <p>
                             Unerdwear cookie liquorice. Cake donut cupcake lollipop soufflé candy. Chocolate oat cake @cheesecake tootsie roll.
                         </p>
